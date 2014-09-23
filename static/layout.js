d22d34a0c0066b81a14d08d55947d1b38a457860
fayching <435462399@163.com>
2014-09-23 18:17:21 +0800

    layout

--- static/layout.js
+++ static/layout.js
@@ -1,5 +1,5 @@
-function write_header(){
-    var template_header = '<div class="head-content">'+
+$(function() {
+  var template_header = '<div class="head-content">'+
         '<div class="title-area">'+
             '<h1>'+
                 '<a href="/">Frozen UI</a>'+
@@ -7,24 +7,27 @@ function write_header(){
         '</div>'+
       
       '<div class="nav-area">'+
+          '<a class="nav-toggle"></a>'+
           '<ol class="main-nav">'+
               '<li><a href="/baseui">CSS</a></li>'+
               '<li><a href="/frozenjs">JavaScript</a></li>'+
-              '<li><a href="/docs/case.html">Animation case</a></li>'+
+              '<li><a href="/docs/case.html">Animation Case</a></li>'+
               '<li><a href="/docs/start.html">Getting started</a></li>'+
               '<li><a href="/docs/about.html">About</a></li>'+
           '</ol>'+
       '</div>'+
     '</div>';
-  
-    document.write(template_header);
-}
-function write_footer(){
-    var template_footer = '<div class="footer-content">'+
-		'<a href="https://github.com/frozenui">GitHub</a> • '+
-        '<a href="http://lab.lepture.com/nico/">nico</a> • '+
-        '<a href="https://github.com/frozenui/frozenui/issues">Issues</a> • '+
-        '<a href="https://github.com/QQVIPTeam">QQVIP FD Team</a>'
-    '</div>';
-    document.write(template_footer);
-}
+  $('.head-area').html(template_header);
+  var template_footer = '<div class="footer-content">'+
+  		'<a href="https://github.com/frozenui">GitHub</a> • '+
+          '<a href="http://lab.lepture.com/nico/">nico</a> • '+
+          '<a href="https://github.com/frozenui/frozenui/issues">Issues</a> • '+
+          '<a href="https://github.com/QQVIPTeam">QQVIP FD Team</a>'
+      '</div>';
+  $('.footer-area').html(template_footer); 
+  $('.nav-toggle').click(function(e){
+    $('.main-nav').animate({
+      height:'toggle'
+    });
+  });
+});
