5cb2d20d61f33a11b38643559c824eae380faeb2
fayching <435462399@163.com>
2014-11-17 15:55:08 +0800

    config

--- nico.js
+++ nico.js
@@ -5,9 +5,13 @@ exports.permalink = "{{filename}}.html"
 // 你可以使用 nodejs 的库哦
 var path = require('path')
 exports.theme = path.join(__dirname, '_themes/one')
-
 exports.writers = [
     'nico.PageWriter',
     'nico.StaticWriter',
     'nico.FileWriter'
 ]
+
+
+// extends for theme usage, that can be accessable by {{config.xxx}}
+var pkg = require(path.join(process.cwd(), 'package.json'))
+exports.package = pkg;
